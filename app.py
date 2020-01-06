0321a9bed3262b167203f77a58725d119e86f560
Francis Tsang <frtsang40@gmail.com>
2020-01-06 15:05:48 -0800

    new routes and forms

--- app.py
+++ app.py
@@ -6,13 +6,31 @@ app = Flask(__name__)
 
 apod_link ='https://api.nasa.gov/planetary/apod?'
 
+image_link = "https://images-api.nasa.gov/search"
+
+earth_imagery = "https://api.nasa.gov/planetary/earth/imagery"
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/apod')
-def apod_image():
+@app.route('/earth')
+def earth():
     return render_template('earth.html')
+
+@app.route('/mars')
+def mars():
+    return render_template('mars.html')
+
+
+@app.route('/earth_search')
+def earth_form():
+    return render_template('imagery_form.html')
+
+@app.route('/earth_imagery')
+def earth_image():
+    latitude = request.args.get('latitude')
+    long
 
 
 if __name__ == '__main__':
