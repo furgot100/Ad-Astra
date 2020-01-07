447910bf388dc6706be33b5cb96c2cb035aa145f
Francis Tsang <frtsang40@gmail.com>
2020-01-07 10:07:23 -0800

    finished path for earth imagery search

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import pprint
 
 app = Flask(__name__)
 
-apod_link ='https://api.nasa.gov/planetary/apod?'
+apod_link ='https://api.nasa.gov/planetary/apod'
 
 image_link = "https://images-api.nasa.gov/search"
 
@@ -30,7 +30,28 @@ def earth_form():
 @app.route('/earth_imagery')
 def earth_image():
     latitude = request.args.get('latitude')
-    long
+    longitude = request.args.get('longitude')
+    date = request.args.get('date')
+
+    API_KEY='iEXQ64MtrOAU8qpIul6IWbSFiohIhil8eJTo2Dvc'
+
+    params = {
+        'lon' : longitude,
+        'lat' : latitude,
+        'date' : date,
+        'api_key' : API_KEY  
+    }
+
+    r = requests.get(earth_imagery, params=params)
+    
+    if not r.status_code == 200:
+        print('error')
+    
+    results = r.json()
+    day = results['date']
+    url = results['url']
+
+    return render_template('earth_img_result.html', day=day, url=url)
 
 
 if __name__ == '__main__':
